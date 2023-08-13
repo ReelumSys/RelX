c240731e6e999e6fe68311e58c5c6735389f991c
ReelumSys <130939312+ReelumSys@users.noreply.github.com>
2023-08-13 04:22:47 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,7 +11,6 @@ from PIL import Image
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot
-import cv2
 
 from PyCrystallography import unit_cell
 from PyCrystallography import lattice
