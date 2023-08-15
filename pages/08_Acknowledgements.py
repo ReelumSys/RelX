44b1ea6238cbda8c02f535145c689dba5818a3c4
ReelumSys <130939312+ReelumSys@users.noreply.github.com>
2023-08-16 00:18:04 +0200

    Update 08_Acknowledgements.py

--- pages/08_Acknowledgements.py
+++ pages/08_Acknowledgements.py
@@ -38,8 +38,13 @@ st.text("")
 st.write("The Rietveld equation and the parameters were modified and are working! However lmfit does a bad job here, as its purpose is to fit through multiple points close together at the x-axis. Ongoing work is done here. Need a conversion to a line graph and still run all parameters. Or you are welcome to try with a very long diffraction time.")
 st.text("")
 st.write("##### Literature")
-st.write("W. H. Hall. (1949) Proc. Phys. Soc. A (London, U. K.) 62 741–743.")
+st.write("Rietveld, H.M. (1969), A profile refinement method for nuclear and magnetic structures. J. Appl. Cryst., 2: 65-71. https://doi.org/10.1107/S0021889869006558")
+st.write("W. H. Hall. (1949) Proc. Phys. Soc. A (London, U. K.) 62 741–743")
+st.write("Flores-Cano, D. A., Chino-Quispe, A. R., Rueda Vellasmin, R., Ocampo-Anticona, J. A., González, J. C., & Ramos-Guivar, J. A. (2021). Fifty years of Rietveld refinement: Methodology and guidelines in superconductors and functional magnetic nanoadsorbents. Revista De Investigación De Física, 24(3), 39-48. https://doi.org/10.15381/rif.v24i3.21028")
 st.write("Izumi, F., Ikeda, T. (2014) - Implementation of the Williamson–Hall and Halder and Wagner Methods into RIETAN-FP")
+st.write("The Rietveld Refinement Method: Half of a Century Anniversary Tomče Runčevski and Craig M. Brown Crystal Growth & Design (2021) 21 (9), 4821-4822 DOI: 10.1021/acs.cgd.1c00854")
+
+
 
 #st.sidebar.markdown("##### vladimirvopravil@hotmail.com")
 
