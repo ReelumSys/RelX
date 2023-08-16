0239028c133451b3a8f0f9a173118137d7a33f30
reel <reelum-prime@outlook.de>
2023-08-16 14:48:54 +0200

    Added my project

--- Main.py
+++ Main.py
@@ -121,7 +121,23 @@ df.to_csv('ksev1.csv', index=False)
 df = pd.read_fwf(name2)
 df.to_csv('ksev1rand.csv', index=False)
 
+# set sidebar collapsed before login
+if 'sidebar_state' not in st.session_state:
+    st.session_state.sidebar_state = 'collapsed'
 
+# hide collapsed control button
+hide_bar = """
+           <style>
+           [data-testid='collapsedControl"] {visibility:hidden;}
+           </style>
+           """
+
+# set sidebar expanded after login
+if login_after:
+    st.session_state.sidebar_state = 'expanded'
+else:
+    st.session_state.sidebar_state = 'collapsed'
+    st.markdown(hide_bar, unsafe_allow_html=True)
 
 os.system("WH.py 1")
 
