dfb3a1787790f6aee95a99c0962cbf1d83fff058
reel <reelum-prime@outlook.de>
2023-08-16 13:37:51 +0200

    Added my project

--- Main.py
+++ Main.py
@@ -55,6 +55,11 @@ import numpy as np
 from lattpy import Lattice
 from lattpy import simple_square
 
+
+    # Clear values from *all* all in-memory and on-disk data caches:
+    # i.e. clear values from both square and cube
+st.cache_data.clear()
+
 #from ... import Charts as weather1
 #from ... import Charts as weather2
 #from ... import Charts as weather3
