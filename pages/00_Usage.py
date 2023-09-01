791aab1c8a59e9466f83e084561b3de4a5e83388
reel <reelum-prime@outlook.de>
2023-09-01 03:58:51 +0200

    Added my project

--- pages/00_Usage.py
+++ pages/00_Usage.py
@@ -1,4 +1,4 @@
-# Contents of ~/my_app/pages/page_0.py
+﻿# Contents of ~/my_app/pages/page_0.py
 import streamlit as st
 import streamlit as st
 import pandas as pd
@@ -6,6 +6,9 @@ import plost
 import base64
 import numpy as np
 from PIL import Image
+import streamlit as st
+import random
+import time
 
 im = 'favicon2.png'
 st.set_page_config(
@@ -34,3 +37,43 @@ def set_background(png_file):
 st.write("###### With this app it is possible to do some work on XRD Charts with only minimal amounts of data needed. Since for Rietveld Refinement only atomic parameters and HKL-values are needed. Same for the Bravais crystal structure.")
 st.text("")
 st.write("###### The Charts section is also for comparing data. The Main gets subtracted by the Comparing. The same is done with logarithmic values on the y-axis.")
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
