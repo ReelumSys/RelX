5cf6bedba851f855196b7dfa154b3d5ef51eddd9
reel <reelum-prime@outlook.de>
2023-09-03 16:15:05 +0200

    Added my project

--- pages/00_Usage.py
+++ pages/00_Usage.py
@@ -46,9 +46,9 @@ st.write("###### The Charts section is also for comparing data. The Main gets su
 
 
 st.title("ChatGPT-like clone")
-OPENAI_API_KEY = "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"
+#OPENAI_API_KEY = "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"
 #openai.api_key = st.secrets["OPENAI_API_KEY"]
-openai.api_key = os.getenv(OPENAI_API_KEY)
+openai.api_key = os.getenv("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")
 
 
 if "openai_model" not in st.session_state:
