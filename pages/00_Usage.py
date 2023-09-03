44c3b5cf46cc3e977e201407d30bcafde98d320f
reel <reelum-prime@outlook.de>
2023-09-03 16:44:37 +0200

    Added my project

--- pages/00_Usage.py
+++ pages/00_Usage.py
@@ -40,6 +40,8 @@ def set_background(png_file):
 st.write("###### With this app it is possible to do some work on XRD Charts with only minimal amounts of data needed. Since for Rietveld Refinement only atomic parameters and HKL-values are needed. Same for the Bravais crystal structure.")
 st.text("")
 st.write("###### The Charts section is also for comparing data. The Main gets subtracted by the Comparing. The same is done with logarithmic values on the y-axis.")
+st.text("")
+st.write("Below is an XRD Chatbot, where you can Post a lot of questions about XRD and life.")
 
 
 
