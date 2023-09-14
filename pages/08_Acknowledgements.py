9da2227d079bf88d8a61380fb2d8bb8041489daa
reel <reelum-prime@outlook.de>
2023-09-14 12:28:15 +0200

    Added my project

--- pages/08_Acknowledgements.py
+++ pages/08_Acknowledgements.py
@@ -58,7 +58,7 @@ st.markdown('<div style="text-align: justify;">The Rietveld Refinement Method: H
 
 
 # bootstrap 4 collapse example
-components.html(
+#components.html(
     """
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -88,8 +88,8 @@ components.html(
       </div>
     </div>
     """,
-    height=600,
-)
+#    height=600,
+#)
 
 
 #st.sidebar.markdown("##### vladimirvopravil@hotmail.com")
