3ea27faa6bae36ef1db61443a8d8c3269e79a7c5
reel <reelum-prime@outlook.de>
2023-09-23 12:59:04 +0200

    Added my project

--- powerxrd/main.py
+++ powerxrd/main.py
@@ -283,20 +283,8 @@ class Rietveld:
             #st.dataframe(dfHKL)
         
             global HKL
-            HKL =          dfHKL
+            HKL = dfHKL
             
-                           """ [[1,0,1],      
-                               [1,1,0],         
-                               [0,1,2],     
-                               [1,0,2],
-                               [1,0,4],
-                               [0,3,2],
-                               [3,0,2],
-                               [2,2,0],
-                               [1,2,3],
-                               [2,1,3], 
-                               [2,2,1], 
-                               [1,1,4]] """
 
             self.pars[i].vary = False
 
