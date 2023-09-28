66fd5ab68add216d3ef0a3633fcb71f9b6a363fe
reel <reelum-prime@outlook.de>
2023-09-28 14:33:54 +0200

    Added my project

--- powerxrd/main.py
+++ powerxrd/main.py
@@ -111,7 +111,7 @@ def Rietveld_func(x, HKL, atomic_positions, s, m_K, TwoTheta_M, K, N_j, f_j, M_j
         dfHKL = pd.read_csv('HKL.csv', names=['H', 'K', 'L'], index_col=False)
         dfHKL = np.array([dfHKL])
         dfHKL = np.asarray(dfHKL, dtype = 'int')
-
+        print(dfHKL)
         HKL =                  dfHKL        
         """
 
