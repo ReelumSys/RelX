8bc2fd8c73b17ed4ccae5c4aeac5aaff4ab40047
reel <reelum-prime@outlook.de>
2024-03-29 19:10:12 +0100

    Added my project

--- pages/00_Usage.py
+++ pages/00_Usage.py
@@ -11,6 +11,8 @@ import random
 import time
 import openai
 import os
+import streamlit as st
+from langchain.llms import OpenAI
 
 
 im = 'favicon2.png'
@@ -97,5 +99,19 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
+st.title('🦜🔗 Quickstart App')
 
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
 
