b446ee4334b0647d524b08d7398e6e9715ef40a0
reel <reelum-prime@outlook.de>
2024-04-02 18:11:23 +0200

    Added my project

--- pages/07_Rietveld_Refinement.py
+++ pages/07_Rietveld_Refinement.py
@@ -32,7 +32,7 @@ global AP1
   
 #number = st.number_input('Enter HKL')
 #st.write('The current number is ', number)
-AP1 = st.number_input("Insert a number", value=0,step=float, placeholder="A number between 0 and 1", min_value=0, max_value=1)
+AP1 = st.number_input("Insert a number", value=0, placeholder="A number between 0 and 1", min_value=0, max_value=1, step=float)
 st.write('The current number is ', AP1)
 
 
