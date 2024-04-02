ae204852566dbcb8073336c58a3a78c65ae0406b
reel <reelum-prime@outlook.de>
2024-04-02 17:47:00 +0200

    Added my project

--- pages/07_Rietveld_Refinement.py
+++ pages/07_Rietveld_Refinement.py
@@ -33,8 +33,8 @@ st.sidebar.header('')
   
 #number = st.number_input('Enter HKL')
 #st.write('The current number is ', number)
-number = st.number_input("Insert a number", value=None, placeholder="A number between 0 and 1", min_value=0, max_value=1)
-st.write('The current number is ', number)
+#number = st.number_input("Insert a number", value=None, placeholder="A number between 0 and 1", min_value=0, max_value=1)
+#st.write('The current number is ', number)
 
 
 
