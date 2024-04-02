7139ce5525571dadb19b50626518e214aa15fd81
reel <reelum-prime@outlook.de>
2024-04-03 01:38:32 +0200

    Added my project

--- powerxrd/main.py
+++ powerxrd/main.py
@@ -14,7 +14,8 @@ from scipy.optimize import curve_fit
 
 from lmfit import CompositeModel, Model
 from lmfit.lineshapes import gaussian, step
-#from pages import 07_Rietveld_Refinement*
+from pages.07_Rietveld_Refinement import AP1
+from pages.07_Rietveld_Refinement import AP2
 
 def braggs(twotheta,lmda=1.54):
     '''interplanar spacing "d_hkl" from Braggs law'''
