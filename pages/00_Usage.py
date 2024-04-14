ab7cb5aec85d9c034ac79802b0611c3e2f2a8708
reel <reelum-prime@outlook.de>
2024-04-14 21:48:44 +0200

    Added my project

--- pages/00_Usage.py
+++ pages/00_Usage.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_icon=im,
     layout="wide",
 )
-st.logo("favicon.png")
+st.sidebar.image("./images/favicon.png")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
