126f148410c8f0ceda5b31a99fc4c28f2839aca6
reel <reelum-prime@outlook.de>
2024-04-14 20:37:08 +0200

    Added my project

--- powerxrd/main.py
+++ powerxrd/main.py
@@ -19,7 +19,7 @@ from lmfit.lineshapes import gaussian, step
 #from Rietveld_Refinement import AtomAinY
 #from Rietveld_Refinement import AtomAinZ
 import sys
-sys.path.append('../pages/07_Rietveld_Refinement')
+sys.path.append('../pages')
 from pages.07_Rietveld_Refinement import AtomAinX
 
 #from pages.Rietveld_Refinement import *
