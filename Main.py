6189e0825016b50a4d43119f94d0e96e6ce82fbc
reel <reelum-prime@outlook.de>
2024-04-15 16:20:53 +0200

    Added my project

--- Main.py
+++ Main.py
@@ -107,16 +107,9 @@ st.text("")
 
 
 image = Image.open('./images/Unbenannt.png')
-col1, col2, col3 = st.beta_columns([1,6,1])
-
-with col1:
-st.write("")
-
-with col2:
-st.image(image)
-
-with col3:
-st.write("")
+background = Image.open("./images/Unbenannt.png")
+col1, col2, col3 = st.columns([0.2, 5, 0.2])
+col2.image(background, use_column_width=True)
 
 
 
