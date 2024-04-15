c061e52ab5103a61efebfc36d22ff9ecc77e3e7d
reel <reelum-prime@outlook.de>
2024-04-15 03:10:11 +0200

    Added my project

--- pages/07_Rietveld_Refinement.py
+++ pages/07_Rietveld_Refinement.py
@@ -115,7 +115,7 @@ if not name3:
   st.stop()
 st.success('Done.')
 
-df = name3
+df = pd.read_fwf(name3)
 df.to_csv('ScaleFactor.csv', index=False)
 np.savetxt('ScaleFactor2.csv', df, fmt='%f', delimiter=',')
 
