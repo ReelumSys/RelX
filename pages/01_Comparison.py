b699baaeb33ef27fe73bc0e644567074bc28fcf5
Vladi <vladi@Reel.idk>
2024-07-02 10:59:26 +0200

    RelX+

--- pages/01_Comparison.py
+++ pages/01_Comparison.py
@@ -29,7 +29,7 @@ st.sidebar.markdown("# ")
 #st.sidebar.subheader('Heat map parameter')
 #time_hist_color = st.sidebar.selectbox('Color by', '')
 #st.sidebar.subheader('Donut chart parameter')
-donut_theta = st.sidebar.selectbox('Select data', ('Theta', 'Area2'))
+#donut_theta = st.sidebar.selectbox('Select data', ('Theta', 'Area2'))
 
 dfheat = pd.read_csv('ksev1.csv', names=['\u00b0 2Theta','Int'])
 stocks = pd.read_csv('Area.csv')
