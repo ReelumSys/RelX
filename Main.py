5b07cee116335591b02feaf7d3783b37d4b7be3b
Vladi <vladi@Reel.idk>
2024-07-03 19:54:45 +0200

    RelX

--- Main.py
+++ Main.py
@@ -209,7 +209,7 @@ def set_background(png_file):
 
 
 with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    #st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 
