7c8aabca0b66bdc00b2b3970c6facb3178a207e2
Vladi <vladi@Reel.idk>
2024-10-22 01:01:42 +0200

    RelX

--- pages/08_Acknowledgements.py
+++ pages/08_Acknowledgements.py
@@ -35,9 +35,7 @@ def set_background(png_file):
 
 #st.markdown("### Acknowledgement")
 #st.write("###### Thanks to PowerXRD's work by Andrew Garcia Ph.D (https://github.com/andrewrgarcia/powerxrd) a Williamson-Hall-Plot and other functions were formulated. The Williamson-Hall-Plot is based on the same gaussian FWHM values as the Scherrer calculation. Furthermore a Halder and Wagner-Plot with derived gaussian FWHM values is made with modifications possible from the Literature. The plots should serve as a direct comparison between the three methods.")
-st.markdown('<div style="text-align: justify;">A Halder and Wagner-Plot was formulated. The plots should serve as a direct comparison between the three methods.</div>', unsafe_allow_html=True)
-st.text("")
-st.markdown('<div style="text-align: justify;">The Scherrer Calculation .</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: justify;">A Halder and Wagner-Plot was formulated. </div>', unsafe_allow_html=True)
 st.text("")
 #st.markdown('<div style="text-align: justify;"> Rietveld Equation was modified. </div>', unsafe_allow_html=True)
 #st.text("")
